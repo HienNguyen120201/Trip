7aba5fb452b3b730fa480cb8363f74f14a788b6e
lam.nguyenlamlahai <91271513+hoanglamlahai@users.noreply.github.com>
2022-11-09 01:44:42 +0700

    update homeScreen

--- TripServiceRA/src/screens/HomePage.js
+++ TripServiceRA/src/screens/HomePage.js
@@ -5,7 +5,8 @@ import {
     StyleSheet, 
     ImageBackground,
     Image,
-    Modal
+    Modal,
+    ScrollView
 } from 'react-native';
 // import * as Animatable from 'react-native-animatable';
 // import LinearGradient from 'react-native-linear-gradient';
@@ -15,52 +16,60 @@ import {
 
 const HomePage = () => {
     return (
-        <ImageBackground 
-            source={require('../../asset/Home_background.jpg')} 
-            resizeMode="cover" 
-            style={styles.image}>
+        <View style = {styles.body}>
                 <View style = {styles.view1}>
                     <View style = {styles.view4}>
                         <Image
                             style = {styles.SignInButton} 
                             source = {require('../../asset/SignIn.png')}
+                            
                         />
+                        <Text style = {styles.LeftText} >Sign In</Text>
                     </View>
                     <View style = {styles.view5}>
                         <Image
                             style = {styles.MenuButton} 
                             source = {require('../../asset/Option_menu.png')}
                         />
+                        <Text style = {styles.RightText} >Menu</Text>
                     </View>
                 </View>
                 <View style = {styles.view2}> 
                     <View style = {styles.view7}> 
-                        <Text style={styles.Trip}> Trip</Text>      
-                        <Text style={styles.Slogan}> Enjoy </Text>        
-                        <Text style={styles.Slogan}> your journey </Text>
+                        <Text style={styles.Trip}> Bảng tin</Text>     
                     </View>
                     <View style = {styles.view6}> 
                     <Image
                             style = {styles.friendButton} 
                             source = {require('../../asset/friends.png')}
                         />
+                    <Text style = {styles.LeftText} >Bạn bè</Text>    
                     </View>
                 </View>
                 <View style = {styles.view3}>   
-                    <Image
+                    <View>
+                        <Image
                             style = {styles.friendButton} 
                             source = {require('../../asset/Add_button.png')}
-                        />    
+                        /> 
+                        <Text style = {styles.MassageText} >Tin nhắn</Text> 
+                    </View> 
+                    <View>  
                     <Image
                             style = {styles.MotorButton} 
                             source = {require('../../asset/Motorbike_icon1.png')}
                         />  
+                        <Text style = {styles.CenterText} >Bắt đầu</Text> 
+                    </View>
+                    <View> 
                     <Image
                             style = {styles.friendButton} 
                             source = {require('../../asset/notification.png')}
-                        />                           
+                        /> 
+                        <Text style = {styles.RightText} >Thông báo</Text> 
+                    </View>       
                 </View>
-        </ImageBackground>
+            </View>
     );
     
 };
@@ -69,32 +78,53 @@ const HomePage = () => {
 const styles = StyleSheet.create({
     body: {
         flex:1,
+        color:'#ffffff',
         flexDirection:'column',
     },
     image: {
-        flex: 1,
-        justifyContent: 'center',
-        
+        flex: 5 ,
+        resizeMode: 'cover',
       },
     Text: {
         color:'#00ffff',
         alignItems:'center',
     },
+    LeftText: {
+        color:'#000000',
+        alignItems:'center',
+        marginLeft: 15,
+    },
+    MassageText: {
+        color:'#000000',
+        alignItems:'center',
+        marginLeft: 10,
+    },
+    CenterText: {
+        color:'#000000',
+        alignItems:'center',
+        marginLeft: 25,
+    },
+    RightText: {
+        color:'#000000',
+        alignItems:'center',
+        marginRight: 20,
+    },
     view1:{
         flexDirection:'row',
-        flex: 1,
+        flex: 1.5,
 
     },
     view2:{
-        flex: 3,
+        flex: 5,
     },
     view3:{
         flex: 1.5,
         marginLeft: 30,
         marginRight: 30,
         justifyContent: 'space-between',
-        alignItems:'center',
+        alignItems:'flex-end',
         flexDirection:'row',
+        marginBottom: 30,
     },
     view4:{
         flex: 1,
@@ -142,12 +172,11 @@ const styles = StyleSheet.create({
         resizeMode: 'stretch',
     },
     Trip:{
-        fontSize: 70,
+        fontSize: 30,
         color:'#BE5E0B',
         fontStyle:'italic',
         fontWeight: 'bold',
-        marginTop: 20,
-        marginBottom: 20,
+        marginBottom: 5,
     },
     Slogan:{
         fontSize: 30,
